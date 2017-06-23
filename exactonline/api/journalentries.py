19be9c97ab0967733ec05936f1181733e89edcbd
Karel <karel@warp.local>
2017-06-23 14:00:53 +0200

    General Journal Entries now also retrieve the deferred GJELines

--- exactonline/api/journalentries.py
+++ exactonline/api/journalentries.py
@@ -5,9 +5,27 @@ from ..resource import GET
 class GeneralJournalEntries(Manager):
 	resource = 'generaljournalentry/GeneralJournalEntries'
 
-	def filter(self, **kwargs):
-		ret = Manager.filter(self, **kwargs)  #super(GeneralJournalEntries, self).filter(**kwargs)
-		uri = ret['GeneralJournalEntryLines']['__deferred']['uri']
+	def resolveDeferredLines(self, entry):
+		try:
+			uri = entry['GeneralJournalEntryLines']['__deferred']['uri']
+		except KeyError:
+			return entry
 		lines = self._api.restv1(GET(str(uri)))
-		ret['GeneralJournalEntryLines']['x'] = lines
+		entry['GeneralJournalEntryLines'] = lines
+		return entry
+
+	def get(self, **kwargs):
+		ret = Manager.filter(self, **kwargs)
+		return self.resolveDeferredLines(ret)
+
+	def filter(self, **kwargs):
+		ret = Manager.filter(self, **kwargs)
+		for entry in ret:
+			self.resolveDeferredLines(entry)
+		return ret
+
+	def create(self, info, deferred=False):
+		ret = Manager.create(self, info)
+		if deferred:
+			return self.resolveDeferredLines(ret)
 		return ret
